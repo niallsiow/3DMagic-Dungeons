d3b86299f8bb9e5d6e4df8ba5e03d55978580e8a
niallsiow@yahoo.co.uk <niallsiow@yahoo.co.uk>
2020-12-12 14:41:23 +0000

    added placeholder player, 3rd person camera and movement

---
+++ 3DMagic&Dungeons/Assets/CursorScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+
+    CursorLockMode lockMode;
+
+    private void Awake()
+    {
+        lockMode = CursorLockMode.Locked;
+        Cursor.lockState = lockMode;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 3DMagic&Dungeons/Assets/PlayerBehaviour.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    CharacterController controller;
+
+    public Transform cam;
+
+    public float speed;
+
+    public float turnSmoothTime;
+    float turnSmoothVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= 0.01f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Vector3 inputVector = new Vector3(direction.x, 0, moveDirection.z);
+            controller.Move(moveDirection * speed * Time.deltaTime);
+        }
+    }
+}
